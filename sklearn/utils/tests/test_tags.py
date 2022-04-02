a6f9afa4f9926b3e41fe9412bb321c403e2fab1c
Victor Ko <vk07275@gmail.com>
2022-04-02 18:04:55 -0400

    Added testcases

--- sklearn/utils/tests/test_tags.py
+++ sklearn/utils/tests/test_tags.py
@@ -45,3 +45,112 @@ def test_safe_tags_no_get_tags(estimator, key, expected_results):
     # check the behaviour of _safe_tags when an estimator does not implement
     # __sklearn_tags__
     assert _safe_tags(estimator, key=key) == expected_results
+
+### Testing __sklearn_tags__() functionality
+
+def test_sklearn_tag1():
+    # simple test with adding one parameter to subclass of BaseEstimator 
+    # as intended to be used
+    newtag = "requires_y"
+    newtagval = True
+    class ACustomEstimator(BaseEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag] = newtagval
+            return tags
+    testtags = ACustomEstimator().__sklearn_tags__()
+    assert newtag in testtags and newtagval == testtags[newtag]
+
+def test_sklearn_tag_multi():
+    # simple test with adding multiple parameters to subclass of BaseEstimator 
+    # as intended to be used
+    newtag1 = "requires_y"
+    newtagval1 = True
+    newtag2 = "requires_z"
+    newtagval2 = True
+    class ACustomEstimator(BaseEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1] = newtagval1
+            tags[newtag2] = newtagval2        
+            return tags
+    testtags = ACustomEstimator().__sklearn_tags__()
+    flag = newtag1 in testtags and newtagval1 == testtags[newtag1]
+    flag and newtag2 in testtags and newtagval2 == testtags[newtag2]
+
+    assert flag
+
+def test_sklearn_tag_extendtags():
+    # simple test with extending tags of a subclass of BaseEstimator 
+    newtag1 = "requires_y"
+    newtagval1 = True
+    newtagval2 = True
+    class ACustomEstimator(BaseEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1] = [newtagval1]
+            return tags
+    class ACustomEstimator2(ACustomEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1].append(newtagval2)
+            return tags            
+    testtags = ACustomEstimator2().__sklearn_tags__()
+    flag = newtag1 in testtags and newtagval1 in testtags[newtag1]
+    flag and newtagval2 in testtags[newtag1]
+
+    assert flag
+
+def test_sklearn_tag_multiInheritance():
+    # check if tags exist in subclasses of the Mixins 
+    newtag1 = "requires_y"
+    newtagval1 = True
+    newtag2 = "requires_z"
+    newtagval2 = True
+    class ACustomMixin1(BaseEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1] = newtagval1
+            return tags
+    class ACustomMixin2(ACustomMixin1):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag2] = newtagval2
+            return tags
+    testtags = ACustomMixin2().__sklearn_tags__()
+    flag = newtag1 in testtags and newtagval1 == testtags[newtag1]
+    flag and newtag2 in testtags and newtagval2 == testtags[newtag2]
+
+    assert flag
+
+def test_sklearn_tag_multiInheritance():
+    # test overwriting tags
+    newtag1 = "requires_y"
+    newtagval1 = True
+    newtagval2 = False
+    class ACustomMixin1(BaseEstimator):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1] = newtagval1
+            return tags
+    class ACustomMixin2(ACustomMixin1):
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags[newtag1] = newtagval2
+            return tags
+    testtags = ACustomMixin2().__sklearn_tags__()
+    flag = newtag1 in testtags and newtagval2 == testtags[newtag1]
+    assert flag
+
+def test_safe_tag():
+    # test _safe_tag with another non BaseEstimator
+    newtag1 = "requires_y"
+    newtagval1 = True
+    class NoBaseEstimator:
+        def __sklearn__tags__(self):
+            return {newtag1: newtagval1}
+
+    testtags = _safe_tags(NoBaseEstimator())
+    flag = newtag1 in testtags and newtagval1 == testtags[newtag1]
+    assert flag
+
